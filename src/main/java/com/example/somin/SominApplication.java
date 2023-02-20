ee1f6432179ff092770e34c7e3fa11b6b9b42527
euiyong <kkey21c@naver.com>
2023-02-20 17:30:38 +0900

    init

---
+++ src/main/java/com/example/somin/SominApplication.java
@@ -0,0 +1,13 @@
+package com.example.somin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SominApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SominApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/somin/SominApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.somin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SominApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
